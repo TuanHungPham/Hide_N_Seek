2992699d38b3d15254c476391f87554b3ce34b41
user <user>
2023-10-25 20:36:54 +0700

    - Adding Profile UI

---
+++ Assets/_Assets/Scripts/UI/ProfileUI/ProfilePanelUI.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class ProfilePanelUI : MonoBehaviour
+{
+    [Header("TEXT")] [SerializeField] private TMP_Text _levelText;
+    [SerializeField] private TMP_Text _foundPlayersText;
+    [SerializeField] private TMP_Text _rescuedPlayersText;
+    [SerializeField] private TMP_InputField _nameInput;
+    private InGameManager InGameManager => InGameManager.Instance;
+
+    private void OnEnable()
+    {
+        SetupUI();
+    }
+
+    private void SetupUI()
+    {
+        _levelText.text = InGameManager.GetAchievementPointData(eAchievementDataType.LEVEL).ToString();
+        _foundPlayersText.text = InGameManager.GetAchievementPointData(eAchievementDataType.FOUND_PLAYERS).ToString();
+        _rescuedPlayersText.text = InGameManager.GetAchievementPointData(eAchievementDataType.RESCUED_PLAYERS).ToString();
+    }
+}
