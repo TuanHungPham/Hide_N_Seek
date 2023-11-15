9486138d8f3bc4591d947621b58fced7a6c49ab7
user <user>
2023-11-15 21:20:53 +0700

    - Handling new Conclusion System

--- Assets/_Assets/Scripts/UI/ConclusionUI/ConclusionPanel.cs
+++ Assets/_Assets/Scripts/UI/ConclusionUI/ConclusionPanel.cs
@@ -1,12 +1,17 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ConclusionPanel : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _rescuedCoin;
+    [SerializeField] private string _rescuedBonusText;
+    [SerializeField] private string _caughtBonusText;
+    [SerializeField] private TMP_Text _bonusCoin;
     [SerializeField] private TMP_Text _collectedCoin;
+    [SerializeField] private TMP_Text _bonusText;
 
     private GameplayManager GameplayManager => GameplayManager.Instance;
+    private GameplaySystem GameplaySystem => GameplaySystem.Instance;
 
     private void OnEnable()
     {
@@ -15,10 +20,19 @@ public class ConclusionPanel : MonoBehaviour
 
     private void SetupUI()
     {
-        long rescuedCoin = GameplayManager.GetCoin(eAddingCoinType.RESCUE_BONUS_COIN);
+        if (GameplaySystem.IsSeekerGameplay())
+        {
+            _bonusText.text = _caughtBonusText;
+        }
+        else
+        {
+            _bonusText.text = _rescuedBonusText;
+        }
+
+        long bonusCoin = GameplayManager.GetCoin(eAddingCoinType.INTERACTING_BONUS_COIN);
         long collectedCoin = GameplayManager.GetCoin(eAddingCoinType.PICK_UP_COIN);
 
-        _rescuedCoin.text = rescuedCoin.ToString();
+        _bonusCoin.text = bonusCoin.ToString();
         _collectedCoin.text = collectedCoin.ToString();
     }
 }
